b67cae4cffc8dbf7ce0aa8c3e513df5d14b53063
yohei.kanehara <yohei.kanehara@vodori.com>
2017-03-12 00:13:22 -0600

    2

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react';
+import Main from './components/Main';
+
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+render(<Main/>, document.getElementById('root'));
