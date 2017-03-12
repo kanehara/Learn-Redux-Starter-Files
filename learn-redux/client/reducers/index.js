e4ff95885106a59f5c0e2a0c5c04157dec27c4cc
yohei.kanehara <yohei.kanehara@vodori.com>
2017-03-12 00:54:27 -0600

    7

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -1,1 +1,9 @@
-bla;sldf
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({posts, comments, router, routerReducer});
+
+export default rootReducer;
